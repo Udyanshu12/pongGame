8bbb5b3a874b19b3ea6be79ff7cb88a40c2b63fa
udyanshu <udyanshu.tiwari@ksolves.com>
2022-08-25 14:44:00 +0530

    Completed

---
+++ main.py
@@ -0,0 +1,52 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreBoard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.title("My Pong Game")
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0), "blue")
+l_paddle = Paddle((-350, 0), "red")
+ball = Ball()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 385:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -385:
+        ball.reset_position()
+        scoreboard.r_point()
+
+    if scoreboard.l_score == 10 or scoreboard.r_score == 10:
+        game_is_on = False
+        if scoreboard.l_score > scoreboard.r_score:
+            print("Red Won")
+        else:
+            print("Blue Won")
+
+screen.exitonclick()
